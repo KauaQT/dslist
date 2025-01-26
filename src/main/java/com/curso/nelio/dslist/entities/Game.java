ae4e35297f44595a43b96518dda35157642d4c21
KauaQT <kaua.teles@sptech.school>
2025-01-26 14:54:18 -0300

    população de dados no banco (import.sql)

--- src/main/java/com/curso/nelio/dslist/entities/Game.java
+++ src/main/java/com/curso/nelio/dslist/entities/Game.java
@@ -16,9 +16,14 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String plataform;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     // Construtor vazio
@@ -26,16 +31,16 @@ public class Game {
 
     }
 
-    public Game(String longDescription, String shortDescription, String imgUrl, String plataform,
-                String genre, Integer year, String title, Long id) {
-        this.longDescription = longDescription;
-        this.shortDescription = shortDescription;
-        this.imgUrl = imgUrl;
-        this.plataform = plataform;
-        this.genre = genre;
-        this.year = year;
-        this.title = title;
+    public Game(Long id, String title, Integer year, String genre, String plataform, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
+        this.title = title;
+        this.year = year;
+        this.genre = genre;
+        this.platforms = plataform;
+        this.score = score;
+        this.imgUrl = imgUrl;
+        this.shortDescription = shortDescription;
+        this.longDescription = longDescription;
     }
 
     public Long getId() {
@@ -70,12 +75,20 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlataform() {
-        return plataform;
+    public String getPlataforms() {
+        return platforms;
     }
 
-    public void setPlataform(String plataform) {
-        this.plataform = plataform;
+    public void setPlataforms(String plataforms) {
+        this.platforms = plataforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     public String getImgUrl() {
